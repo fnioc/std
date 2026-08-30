e1b96d29b869cf030e57338553b7a16788200c06
Thomas Butler <2511516+fnrhombus@users.noreply.github.com>
2026-08-30 16:30:50 -0400

    refactor(primitives): isAllThere tests presence with isDefined, point-free

--- libraries/primitives/src/toolkit/is-all-there.ts
+++ libraries/primitives/src/toolkit/is-all-there.ts
@@ -1,4 +1,6 @@
+import { isDefined } from '@rhombus-toolkit/type-guards';
+
 export function isAllThere<T>(items: Array<T | undefined>): items is T[];
 export function isAllThere<T>(items: Iterable<T | undefined>): items is Iterable<T> {
-  return Iterator.from(items).every(item => item !== undefined);
+  return Iterator.from(items).every(isDefined);
 }
